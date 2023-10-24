3ea5b29756793f03c739aa0332a6f9c856346d93
waleedCodes3 <waleedcodes03@gmail.com>
2023-10-24 17:18:45 +0500

    home fragment in progress

---
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.foodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    //    private  var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        binding=ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navController = Navigation.findNavController(this, R.id.navHostFragment)
+        NavigationUI.setupWithNavController(navigationView, navController)
+    }
+}
